cb92126c6dd6a745568d4134c4bdfc6383c4a2e2
Bruno Castro <bruno.castro@codeminer42.com>
2018-04-11 16:03:56 -0300

    feat: Add Book component

---
+++ src/Book.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Book = ({ title, cover, authors }) => (
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${cover})` }}></div>
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{title}</div>
+    <div className="book-authors">{authors}</div>
+  </div>
+)
+
+export default Book
